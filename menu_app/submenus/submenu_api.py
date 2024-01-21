2f7e262f84d798806ff59ced1dbd38134c31eb9e
Karina Abdullaeva <abdullaeva.karina.r@gmail.com>
2024-01-22 00:40:55 +0300

    main: Add submenus package with its api and crud

---
+++ menu_app/submenus/submenu_api.py
@@ -0,0 +1,88 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from .. import schemas
+from ..database import SessionLocal
+from . import submenu_crud
+
+submenu_router = APIRouter(prefix="/api/v1/menus/{menu_id}/submenus")
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@submenu_router.get("/", response_model=list[schemas.SubmenuRead])
+def get_all_submenus(menu_id: str, skip: int = 0,
+                     limit: int = 100,
+                     db: Session = Depends(get_db)):
+    return submenu_crud.read_submenus(db, menu_id, skip=skip, limit=limit)
+
+
+@submenu_router.post("/", response_model=schemas.SubmenuRead, status_code=201)
+def post_submenu(submenu: schemas.SubmenuCreate,
+                 menu_id: str,
+                 db: Session = Depends(get_db)):
+    db_submenu = submenu_crud.get_submenu_by_title(db=db,
+                                                   submenu_title=submenu.title)
+    if db_submenu:
+        raise HTTPException(
+            status_code=400,
+            detail="A submenu with that title already exists"
+        )
+    return submenu_crud.create_submenu(db=db, menu_id=menu_id, submenu=submenu)
+
+
+@submenu_router.get("/{submenu_id}", response_model=schemas.SubmenuRead)
+def get_submenu(submenu_id: str, db: Session = Depends(get_db)):
+    db_submenu = submenu_crud.get_submenu_by_id(db, submenu_id=submenu_id)
+    if db_submenu is None:
+        raise HTTPException(
+            status_code=404,
+            detail="submenu not found"
+        )
+    return db_submenu
+
+
+@submenu_router.patch("/{submenu_id}", response_model=schemas.SubmenuRead)
+def patch_submenu(submenu_id: str,
+                  updated_submenu: schemas.MenuCreate,
+                  db: Session = Depends(get_db)):
+    current_submenu = submenu_crud.get_submenu_by_id(db, submenu_id=submenu_id)
+    updated_title = submenu_crud.get_submenu_by_title(
+        db, submenu_title=updated_submenu.title
+    )
+    if current_submenu is None:
+        raise HTTPException(
+            status_code=404,
+            detail="submenu not found",
+        )
+    elif updated_title:
+        raise HTTPException(
+            status_code=400,
+            detail="A submenu with that title already exists"
+        )
+
+    return submenu_crud.update_submenu(db=db,
+                                       current_submenu=current_submenu,
+                                       updated_submenu=updated_submenu)
+
+
+@submenu_router.delete("/{submenu_id}",
+                       response_model=list[schemas.SubmenuRead])
+def delete_submenu(menu_id: str, submenu_id: str,
+                   db: Session = Depends(get_db)):
+    db_submenu = submenu_crud.get_submenu_by_id(db, submenu_id=submenu_id)
+    if db_submenu is None:
+        raise HTTPException(
+            status_code=404,
+            detail="submenu not found",
+        )
+    return submenu_crud.delete_submenu(db=db,
+                                       menu_id=menu_id,
+                                       submenu_id=submenu_id)
---
+++ menu_app/submenus/submenu_crud.py
@@ -0,0 +1,44 @@
+from sqlalchemy.orm import Session
+
+from .. import schemas
+from ..models import Submenu
+
+
+def create_submenu(db: Session, menu_id: str, submenu: schemas.SubmenuCreate):
+    new_submenu = Submenu(title=submenu.title,
+                          description=submenu.description,
+                          menu_id=menu_id)
+    db.add(new_submenu)
+    db.commit()
+    db.refresh(new_submenu)
+    return new_submenu
+
+
+def read_submenus(db: Session, menu_id: str, skip: int = 0, limit: int = 100):
+    return db.query(Submenu).filter(Submenu.menu_id == menu_id).offset(skip).limit(limit).all()
+
+
+def get_submenu_by_title(db: Session, submenu_title: str):
+    return db.query(Submenu).filter(Submenu.title == submenu_title).first()
+
+
+def get_submenu_by_id(db: Session, submenu_id: str):
+    return db.query(Submenu).filter(Submenu.id == submenu_id).first()
+
+
+def update_submenu(db: Session,
+                   current_submenu: schemas.SubmenuRead,
+                   updated_submenu: schemas.SubmenuCreate):
+    current_submenu.title = updated_submenu.title
+    current_submenu.description = updated_submenu.description
+    db.merge(current_submenu)
+    db.commit()
+    db.refresh(current_submenu)
+    return current_submenu
+
+
+def delete_submenu(db: Session, menu_id: str, submenu_id: str):
+    menu = get_submenu_by_id(db=db, submenu_id=submenu_id)
+    db.delete(menu)
+    db.commit()
+    return read_submenus(db=db, menu_id=menu_id)
