bdd422445127d8b0a73f56e188624cf767f58f95
Karina Abdullaeva <abdullaeva.karina.r@gmail.com>
2024-01-22 16:22:13 +0300

    main: Add submenus count

--- menu_app/models.py
+++ menu_app/models.py
@@ -3,9 +3,22 @@ from uuid import uuid4
 from sqlalchemy import Column, ForeignKey, String, Text
 # https://postgrespro.ru/docs/postgresql/9.5/datatype-uuid
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, column_property
+from sqlalchemy.sql import select, func
 
 from .database import Base
+
+
+class Submenu(Base):
+    """Модель подменю."""
+
+    __tablename__ = 'submenus'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    title = Column(String(50), nullable=False, unique=True)
+    description = Column(Text)
+    menu_id = Column(UUID(as_uuid=True), ForeignKey('menus.id'))
+    menu = relationship("Menu", back_populates="submenus")
 
 
 class Menu(Base):
@@ -19,15 +32,7 @@ class Menu(Base):
     submenus = relationship('Submenu',
                             back_populates="menu",
                             cascade='all, delete')
-
-
-class Submenu(Base):
-    """Модель подменю."""
-
-    __tablename__ = 'submenus'
-
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
-    title = Column(String(50), nullable=False, unique=True)
-    description = Column(Text)
-    menu_id = Column(UUID(as_uuid=True), ForeignKey('menus.id'))
-    menu = relationship("Menu", back_populates="submenus")
+    submenus_count = column_property(
+        select(func.count(Submenu.id)).where(
+            Submenu.menu_id == id).correlate_except(Submenu).as_scalar()
+    )
