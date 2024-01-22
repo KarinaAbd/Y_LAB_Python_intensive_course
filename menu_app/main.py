6acf44e7d8e04c59c5bcf61ed1b7b71b54c8f0f7
Karina Abdullaeva <abdullaeva.karina.r@gmail.com>
2024-01-22 21:47:50 +0300

    main: Refactor imports

--- menu_app/main.py
+++ menu_app/main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
 
+from .database import Base, engine
+from .dishes.dish_api import dish_router
 from .menus.menu_api import menu_router
 from .submenus.submenu_api import submenu_router
 
-from . import models
-from .database import engine
-
-models.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(menu_router)
 app.include_router(submenu_router)
+app.include_router(dish_router)
